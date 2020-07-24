8bd983cb4b343a57730c0287d0dd5968bd285c2e
zhenekq <zhenek02ss@gmail.com>
2020-07-24 10:48:13 +0300

    4th branching program

---
+++ Basics of software code development/branching programs/4/src/branchingProgram4/Main.java
@@ -0,0 +1,23 @@
+package branchingProgram4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = sc.nextInt();
+		
+		if(x * y <= A * B) {
+			System.out.println("Brick will pass");
+		}else {
+			System.out.println("Brick will not pass");
+		}
+		
+	}
+
+}
