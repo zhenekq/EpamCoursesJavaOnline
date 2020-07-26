c1abe1b80adcb44dfd717e40eb67558eca6ef65b
zhenekq <zhenek02ss@gmail.com>
2020-07-26 16:02:25 +0300

    6th

--- Basics of software code development/cycle programs/3/src/cycleProgram3/Main.java
+++ Basics of software code development/cycle programs/3/src/cycleProgram3/Main.java
@@ -2,4 +2,12 @@ package cycleProgram3;
 
 public class Main {
 
+	public static void main(String[] args) {
+		long result = 1;
+		for(int i=1;i<=10;i++) {
+			result*=i*i;
+		}
+		System.out.println(result);
+	}
+
 }
---
+++ Basics of software code development/cycle programs/6/src/cycleProgram6/Main.java
@@ -0,0 +1,14 @@
+package cycleProgram6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=255;i++) {
+			char ch = (char)i;
+			int f = ch;
+			System.out.println(ch+" = " + f);
+			
+		}
+	}
+
+}
