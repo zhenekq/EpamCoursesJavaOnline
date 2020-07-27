dda95f01a265595c47e671af75e6d5a1ec7b58e6
zhenekq <zhenek02ss@gmail.com>
2020-07-27 14:15:14 +0300

    5th algo array prog

---
+++ Algorithmization/One-dimensional array/5/src/OneDimensionalArray5/Main.java
@@ -0,0 +1,21 @@
+package OneDimensionalArray5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int  array[] = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			array[i] = sc.nextInt();
+			if(array[i]>i) {
+				System.out.print(array[i]+" ");
+			}	
+		}
+	}
+
+}
